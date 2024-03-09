b13237cf0f7e52fd914738b3f660ee777bc0e375
Augusto Chaves Fiore <augustochavesfiore@gmail.com>
2024-03-09 00:10:28 -0300

    Agrega middleware de verificación de firma de GitHub y actualiza variables de entorno

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import { envs } from './config';
 import { GithubController } from './presentation/github/controller';
+import { GithubSha256Middleware } from './presentation/middleware/github-sha256.middleware';
 
 
 function main(){
@@ -8,11 +9,11 @@ function main(){
     const port= envs.PORT;
 
     app.use(express.json());
-    app.use(express.urlencoded({extended:true}))
+    app.use(express.urlencoded({extended:true}));
+    app.use(GithubSha256Middleware.verifySignature);
 
     const controller = new GithubController();
     app.post('/api/github', controller.webhookHandler)
-
 
     app.listen(port,()=>{
         console.log(`Server running on http://localhost:${port}`);
